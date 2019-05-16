3145556190ee587f8d573f91c4ed70ead33cfbce
Adam Tannir <atannir@gmail.com>
2019-05-16 15:20:42 -0400

    First dashboard commit

---
+++ Web/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=en>
+  <head>
+    <title>FUBAR Labs Status Monitor Dashboard</title>
+    <link href="dashboard.css" rel="stylesheet">
+    <script src="https://d3js.org/d3.v5.min.js"></script>
+  </head>
+  <body>
+<?php
+
+# Large frame with current status
+# Graphs underneath for historical data from the webservices
+
+
+
+
+
+?>
+  </body>
+</html>
